bc55ac27b24be4325f3fdb3f8e2a1bb0dd713c46
Mars Hamza <hamza31mars@gmail.com>
2025-03-05 13:35:39 +0100

    email unique

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,4 @@ const userSchema = mongoose.Schema({
   password: { type: String, required: true }
 });
 
-// Ajout d'un index unique pour éviter les doublons même en cas d'erreur Mongoose
-userSchema.index({ email: 1 }, { unique: true });
-
 module.exports = mongoose.model('User', userSchema);
